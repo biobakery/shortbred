ecc0fb2d6d9440bdf0f98c2d24f4cae37452a478
jimkaminski <jimkaminski@localhost>
2012-11-11 13:39:32 -0500

    Changed search strategy - get all usearch hits above id, filter by alignment length

--- shortbred_quantify.py
+++ shortbred_quantify.py
@@ -22,16 +22,14 @@ parser.add_argument('--wgs', type=str, dest='sWGS', help='Enter the path and nam
 parser.add_argument('--results', type=str, dest='sResults', help='Enter the path and name of the results file.')
 parser.add_argument('--blastout', type=str, dest='strBlast', help='Enter the path and name of the blastoutput.')
 
- 
-
 #Parameters
 parser.add_argument('--id', type=float, dest='dID', help='Enter the percent identity for the match', default = .95)
-parser.add_argument('--alnlength', type=int, dest='iAlnLength', help='Enter the minimum length for a valid alignment. The default is 20.', default = 20)
+parser.add_argument('--alnlength', type=int, dest='iAlnLength', help='Enter the minimum alignment length. The default is 20', default = 20)
 
 
 parser.add_argument('--tmp', type=str, dest='sTmp', default =os.getcwd() +os.sep + "tmp",help='Enter the path and name of the tmp directory.')
 parser.add_argument('--length', type=int, dest='iLength', help='Enter the minimum length of the markers.')
-parser.add_argument('--threads', type=int, dest='iThreads', help='Enter the number of CPUs available for usearch.')
+parser.add_argument('--threads', type=int, dest='iThreads', help='Enter the number of CPUs available for usearch.', default=1)
 parser.add_argument('--notmarkers', type=str, dest='strNM',default="N", help='.')
 
 #DB Note - Maybe ask Nicola how to remove usearch6 output
@@ -40,11 +38,15 @@ parser.add_argument('--notmarkers', type=str, dest='strNM',default="N", help='.'
 args = parser.parse_args()
 
 dirTmp = args.sTmp
+if not os.path.exists(dirTmp):
+    os.makedirs(dirTmp)
 
-log = open(dirTmp + os.sep +"Quantlog.txt", "w")
+strBase = os.path.splitext(os.path.basename(args.sMarkers))[0]
+
+log = open(str(dirTmp + os.sep + strBase+ ".log"), "w")
 log.write("ShortBRED log \n" + datetime.date.today().ctime() + "\n SEARCH PARAMETERS \n")
 log.write("Match ID:" + str(args.dID) + "\n")
-log.write("Match Coverage of Read:" + str(args.dCov) + "\n")
+log.write("Alignment Length:" + str(args.iAlnLength) + "\n")
 log.close()
 
 ###############################################################################
@@ -75,20 +77,19 @@ p = subprocess.check_call(["usearch6", "--makeudb_usearch", args.sMarkers, "--ou
 
 
 #Use usearch to check for hits (usearch local)
-subprocess.check_call(["usearch6", "--usearch_local", args.sWGS, "--db", strDBName, "--id", str(args.dID),"--blast6out", args.strBlast,"--threads", str(args.iThreads)])
+subprocess.check_call(["usearch6", "--usearch_local", args.sWGS, "--db", strDBName, "--id", str(args.dID),"--blast6out", args.strBlast,"--threads", str(args.iThreads),"--maxaccepts","0","--maxrejects","0"])
 
 
 #Go through the blast hits, for each prot family, print out the number of hits
 dictBLAST = {}    
-for aLine in csv.reader( open( strSearchResults), csv.excel_tab ):
-	#print aLine[1]
-	if args.strNM=="N":
-		mtchProtStub = re.search(r'(.*)_(.M)[0-9]*_\#([0-9]*)',aLine[1])    
-		strProtFamily = mtchProtStub.group(1)
-	else:
-		strProtFamily = aLine[1]
-      if (int(aLine[3])>(args.iAlnLength)):
-          dictBLAST.setdefault(strProtFamily,set()).add((aLine[0]))
+for aLine in csv.reader( open(args.strBlast), csv.excel_tab ):
+	if int(aLine[3])>= args.iAlnLength:
+        	if args.strNM=="N":
+        		mtchProtStub = re.search(r'(.*)_(.M)[0-9]*_\#([0-9]*)',aLine[1])    
+        		strProtFamily = mtchProtStub.group(1)
+        	else:
+        		strProtFamily = aLine[1]
+        	dictBLAST.setdefault(strProtFamily,set()).add((aLine[0]))
 	
     
 for strProt in dictBLAST.keys():
