71f17b8444246e89f9570179b699362421042b3e
jimkaminski <jimkaminski@localhost>
2016-02-07 11:28:34 -0800

    Updated utils/AdjustFastaHeadersForShortBRED.py - Will now convert all bad chars to underscores, and enforce max header length of 240 chars.

--- utils/AdjustFastaHeadersForShortBRED.py
+++ utils/AdjustFastaHeadersForShortBRED.py
@@ -1,7 +1,7 @@
 #########################################################################
 # Jim Kaminski
 # Huttenhower Lab
-# 2/3/2016
+# 2/7/2016
 #########################################################################
 
 
@@ -21,19 +21,24 @@ from argparse import RawTextHelpFormatter
     
 parser = argparse.ArgumentParser(description='This script makes small changes to an input fasta file to format the sequence \
 ids for ShortBRED. It will do the following: \
-    * Convert the first --numspaces (args.iSpace) to "_"\'s \
-    * Add an unique ID when two seqs have the exact same name. \
+    * Add a unique ID when two seqs have the exact same name. \
+    * Cut fasta files with very long names to 251 characters match (240 chars, plus possible "___Copy000X" suffix for duplicates.) \
     * Replace characters like "*,[,:" with _ . ',formatter_class=RawTextHelpFormatter) 
-parser.add_argument("--numspaces", default = 2, type=int, dest='iSpacesToChange')
+#parser.add_argument("--numspaces", default = 2, type=int, dest='iSpacesToChange')
 args = parser.parse_args()
 
 dictHeaderCounts = {}
 reBadChars=re.compile(r'[\\\/\*\=\:\'\[\]\.\;]')
+c_iMaxIDLength = 240
+
 
 for strLine in sys.stdin:
     strLine = strLine.strip()
     if strLine[0]==">":
-        strHeader = strLine[1:].replace(" ","_",args.iSpacesToChange)
+        #strHeader = strLine[1:].replace(" ","_",args.iSpacesToChange)
+        strHeader = strLine[1:].replace(" ","_")
+        if len(strHeader) > c_iMaxIDLength:
+            strHeader = strHeader[0:c_iMaxIDLength+1] 
         strHeader = re.sub(reBadChars,"_",strHeader)
         dictHeaderCounts[strHeader] = dictHeaderCounts.get(strHeader,0)+1
         if dictHeaderCounts[strHeader] > 1:
@@ -41,6 +46,4 @@ for strLine in sys.stdin:
         print ">" + strHeader
     else:
         print strLine
-        
-        
         
